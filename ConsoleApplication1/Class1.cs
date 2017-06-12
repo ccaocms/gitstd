3c18a34006c8fd48c43d8f359cd5ba71302892df
Chris Cao <ccao@cmsonline.com>
2017-06-12 09:05:46 +0800

    initial project

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace CMSTIP
+{
+    public class Common_SocketServer
+    {
+        
+
+        private int port;
+        private static byte[] result = new byte[1024];
+        private static Socket server;
+        private IPAddress ip;
+        private static Socket client;
+        private static Thread myThread;
+        private static Thread receiveThread;
+        //private static int ConnectionCount = 0;
+        private static System.Timers.Timer ClientCheckingTimer;
+
+        private static List<Socket> AllSockets = new List<Socket>();
+        private static Object[,] SocketList = new Object[2, 2];
+
+        private static bool threadblock;//stop thread;
+        private static bool reciveblock;//stop thread;
+
+        public Common_SocketServer(string ipadr, int port)
+        {
+            this.port = port;
+            ip = IPAddress.Parse(ipadr);
+        }
+        public Boolean setConnection()
+        {
+            try
+            {
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                server.Bind(new IPEndPoint(ip, port));
+                server.Listen(1);
+                threadblock = true;
+                reciveblock = true;
+                myThread = new Thread(ListenClientConnect);
+                myThread.Start();
+                //ListenClientConnect();
+
+                Console.WriteLine("Begin listening in port " + port);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("start socket server get exception: " + e.Message);
+                return false;
+            }
+
+            ClientCheckingTimer = new System.Timers.Timer(5000);  // 5000 milliseconds = 5 seconds
+            ClientCheckingTimer.AutoReset = true;
+            ClientCheckingTimer.Elapsed += new System.Timers.ElapsedEventHandler(ClientCheckingTimeout);
+            ClientCheckingTimer.Start();
+
+            return true;
+        }
+
+        private static void ClientCheckingTimeout(object sender, System.Timers.ElapsedEventArgs e)
+        {
+
+
+            Console.WriteLine("begin checking clients, allsockets count: " + AllSockets.Count());
+
+            for (int i = 0; i < AllSockets.Count(); i++)
+            {
+                if (AllSockets[i] == null)
+                {
+                    Console.WriteLine("client is null");
+
+                    ReleaseSocket(AllSockets[i]);
+                }
+                else
+                {
+                    Console.WriteLine("client not null, begin process");
+                    Boolean socketstatus = true;
+                    socketstatus = IsConnected(AllSockets[i]);
+
+                    Console.WriteLine("timeout check socket status: " + socketstatus);
+
+                    if (!socketstatus)
+                    {
+                        Console.WriteLine("socket is not avaliable, begin to release");
+
+
+                        ReleaseSocket(AllSockets[i]);
+
+                    }
+
+                }
+            }
+        }
+
+        private static void ReleaseSocket(Socket socket)
+        {
+            Console.WriteLine("begin socket release processing");
+
+            try
+            {
+                receiveThread.Abort();
+            }
+            catch (Exception ex)
+            {
+                 Console.WriteLine("Thread abort get exception: " + ex.Message);
+            }
+            AllSockets.Remove(socket);
+
+            try
+            {
+                socket.Shutdown(SocketShutdown.Both);
+            }
+            catch (Exception ex)
+            {
+                 Console.WriteLine("socket shutdown get exception: " + ex.Message);
+            }
+
+            try
+            {
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                 Console.WriteLine("socket close get exception: " + ex.Message);
+            }
+
+        }
+
+        public static bool IsConnected(Socket socket)
+        {
+
+            try
+            {
+                bool part1 = socket.Poll(1000, SelectMode.SelectRead);
+                bool part2 = (socket.Available == 0);
+                if (part1 & part2)
+                {//connection is closed
+                    return false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                 Console.WriteLine("IsConnected get excetpion: " + ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        private static void ListenClientConnect()
+        {
+            
+            //Boolean Listening = true;
+            while (threadblock== true)
+            {
+                try
+                {
+                    client = server.Accept();
+                    AllSockets.Add(client);
+                }
+                catch {
+                    Console.WriteLine("server accept() error");
+                    return;
+                }
+                
+               
+
+                //Console.WriteLine("AllSockets count is: " + AllSockets.Count());
+                //do checking first before begin receving data
+
+                if (AllSockets.Count() > 2)
+                {
+                    //if there's too many connection, disconnect the last one
+                    SendMsg(client, "Too many connections");
+                    AllSockets.Remove(client);
+
+                    client.Close();
+                     Console.WriteLine("After disconnect client, now allsockets count is: " + AllSockets.Count());
+                    //Listening = false;
+                }
+                else
+                {
+                    receiveThread = new Thread(ReceiveMessage);
+                    receiveThread.Start(client);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+
+        private static void ReceiveMessage(object clientSocket)
+        {
+            int receiveNumber=0;
+
+            client = (Socket)clientSocket;
+       
+            while (reciveblock==true)
+            {
+
+                if (client != null && client.Connected)
+                {
+                    
+                    Console.WriteLine("client is not null");
+                    //receive data added receive timeout, if exception happend continue
+                    try
+                    {
+                        receiveNumber = client.Receive(result);//获取接收数据的长度
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message + "\r\nshould be OK\r\n");
+                        continue;
+                    }
+
+                    Console.WriteLine("Socket Server get info: " + System.Text.Encoding.Default.GetString(result));
+
+                    
+                    if (receiveNumber > 0)
+                    {
+                        SendMsg(clientSocket, "Hello from Server");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("client is  null!!!!!!");
+                    reciveblock = false;
+                }
+                   
+               
+            }
+        }
+
+        private static void SendMsg(object clientSocket, String msg)
+        {
+            client = (Socket)clientSocket;
+
+            try
+            {
+                client.Send(Encoding.ASCII.GetBytes(msg));
+            }
+            catch (Exception e)
+            {
+                 Console.WriteLine("Sending message to socket client get exception: " + e.Message);
+            }
+        }
+
+        public static void ReleaseServer()
+        {
+            if (server != null)
+            {
+                try
+                {
+                    if (client!=null)
+                    {
+                        client.Shutdown(SocketShutdown.Both);
+                        Console.WriteLine("commer server shutdown");
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("commer server shutdown" + e.Message);
+                }
+                if (ClientCheckingTimer != null)
+                {
+                    ClientCheckingTimer.Stop();
+                    ClientCheckingTimer.Close();
+                }
+                if (receiveThread!=null)
+                {
+                    reciveblock = false;
+                    Thread.Sleep(6000);
+                    receiveThread.Abort();
+                    client = null;
+                    threadblock = false;
+
+                    Thread.Sleep(6000);
+                }
+                
+                 // myThread.Abort();
+                try
+                {
+                    server.Close();
+                }
+                catch (Exception e)
+                {
+                     Console.WriteLine("commer server close error" + e.Message);
+                }
+
+            }
+
+        }
+
+    }
+
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using CMSTIP;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static string ConfigFile = "c:\\Logs\\config.xml";
+        private static void updateMonerisConfig(String key, String value)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            //String configName = @"c:\cms slyce1\chipdna.config.xml";
+            Boolean result = false;
+
+            try
+            {
+                xmlDoc.Load(ConfigFile);//loading config xml file
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error" + e.Message);
+            }
+
+            XmlNode xns = xmlDoc.SelectSingleNode("CTIS");//get root node
+            XmlNodeList xnl = xns.ChildNodes;//get all child nodes
+            foreach (XmlNode xn in xnl)
+            {
+                if (xn.Name == "Moneris")
+                {
+                    XmlNodeList xnl2 = xn.ChildNodes;//get all child nodes
+                    foreach (XmlNode xn2 in xnl2)
+                    {
+                        if (xn2.Name.Equals(key))
+                        {
+                            xn2.InnerText = value;
+                            xmlDoc.Save(ConfigFile);
+                            result = true;
+                        }
+                    }
+                }
+
+            }
+            if (!result)
+            {
+                XmlElement xe1 = xmlDoc.CreateElement("Moneris");//创建一个<book>节点 
+                XmlElement xesub1 = xmlDoc.CreateElement("TerminalIP");
+                if (key == "TerminalIP")
+                {
+                    xesub1.InnerText = value;
+                }
+              
+                XmlElement xesub2 = xmlDoc.CreateElement("TerminalPort");
+                if (key=="TerminalPort")
+                {
+                    xesub1.InnerText = value;
+                }
+                xe1.AppendChild(xesub1);
+                xe1.AppendChild(xesub2);
+
+
+                xns.AppendChild(xe1);
+                xmlDoc.Save(ConfigFile);     
+
+            }
+
+        }
+        public static void SetServiceConfig(JObject inPar)
+        {
+            Dictionary<string, string> htmlAttributes = JsonConvert.DeserializeObject<Dictionary<string, string>>(inPar.ToString());
+            foreach (var vv in htmlAttributes)
+            {
+                string key = vv.Key;
+                string s = vv.Value;
+                Console.WriteLine(" " + vv.Key + " " + vv.Value);
+                UpdateServiceConfigXml(key, s);
+            }
+        }
+        private static void UpdateServiceConfigXml(String key, String value)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            //String configName = @"c:\cms slyce1\chipdna.config.xml";
+            if (key == "TerminalIP" || key == "TerminalPort")
+            {
+                updateMonerisConfig(key, value);
+                return;
+            }
+            try
+            {
+                xmlDoc.Load(ConfigFile);//loading config xml file
+            }
+            catch (Exception e)
+            {
+                // Console.WriteLine("Open config.xml get exception: " + e.Message);
+            }
+
+            XmlNode xns = xmlDoc.SelectSingleNode("CTIS");//get root node
+            XmlNodeList xnl = xns.ChildNodes;//get all child nodes
+            foreach (XmlNode xn in xnl)
+            {
+                switch (xn.Name)
+                {
+                    case "SocketIP":
+                    case "SocketPort":
+                    case "Secure":
+                    case "Cert":
+                    case "PWD":
+                    case "RootCert":
+                    case "Delete":
+                    case "Store":
+                    case "FirefoxCert":
+                    case "FirefoxPWD":
+                    case "FirefoxRootCert":
+                    case "UsingFirefox":
+                    case "AutoDisconnect":
+                    case "AutoDisconnectTimeout":
+                    case "COMWriteTimeout":
+                    case "COMReadTimeout":
+                    case "EnableCheckServerLoop":
+                    case "CheckServerInterval":
+                        if (xn.Name.Equals(key))
+                        {
+                            xn.InnerText = value;
+                            xmlDoc.Save(ConfigFile);
+                        }
+
+                        break;
+                    default:
+                       //  Console.WriteLine("Key " + xn.Name + " is not supported in config.xml");
+                        break;
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            /*Program.updateMonerisConfig("TerminalIP", "192.168.0.3");
+            Program.updateMonerisConfig("TerminalPort", "1855");*/
+            string v = "{\"TerminalIP\":\"10.0.0.200\",\"TerminalPort\":\"8180\",\"UsingFirefox\":\"false\"}";
+            JObject x = JObject.Parse(v);
+            //Program.SetServiceConfig(x);
+
+            Common_SocketServer cs = new Common_SocketServer("127.0.0.1", 33333);
+            cs.setConnection();
+
+            Console.ReadLine();
+            Common_SocketServer.ReleaseServer();
+
+        }
+    }
+}
